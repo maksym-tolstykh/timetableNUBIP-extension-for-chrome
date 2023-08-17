14cdaebcba8795a344e5be1dc5b30ed1ee55a8df
Tolstykh Maksym <mrymnik3d@gmail.com>
2023-08-17 12:51:18 +0300

    fix error and add id for google account

--- main.js
+++ main.js
@@ -84,51 +84,66 @@ const parseTime = () => {
     else if (currentDay >= new Date(currentDay).setHours(10, 10, 0, 0) &&
         currentDay <= new Date(currentDay).setHours(11, 30, 0, 0)) {
         currentLesson = 2;
-        allTime[1].style.backgroundColor = "#94ff00";
+        allTime[1]?.style.backgroundColor = "#94ff00";
     }
     else if (currentDay >= new Date(currentDay).setHours(11, 50, 0, 0) &&
         currentDay <= new Date(currentDay).setHours(13, 10, 0, 0)) {
         currentLesson = 3;
-        allTime[2].style.backgroundColor = "#94ff00";
+        allTime[2]?.style.backgroundColor = "#94ff00";
     }
     else if (currentDay >= new Date(currentDay).setHours(13, 30, 0, 0) &&
         currentDay <= new Date(currentDay).setHours(14, 50, 0, 0)) {
         currentLesson = 4;
-        allTime[3].style.backgroundColor = "#94ff00";
+        allTime[3]?.style.backgroundColor = "#94ff00";
     }
     else if (currentDay >= new Date(currentDay).setHours(15, 10, 0, 0) &&
         currentDay <= new Date(currentDay).setHours(16, 30, 0, 0)) {
         currentLesson = 5;
-        allTime[4].style.backgroundColor = "#94ff00";
+        allTime[4]?.style.backgroundColor = "#94ff00";
     }
     else if (currentDay >= new Date(currentDay).setHours(16, 50, 0, 0) &&
         currentDay <= new Date(currentDay).setHours(18, 10, 0, 0)) {
         currentLesson = 6;
-        allTime[5].style.backgroundColor = "#94ff00";
+        allTime[5]?.style.backgroundColor = "#94ff00";
     }
     else if (currentDay >= new Date(currentDay).setHours(18, 30, 0, 0) &&
         currentDay <= new Date(currentDay).setHours(19, 50, 0, 0)) {
         currentLesson = 7;
-        allTime[6].style.backgroundColor = "#94ff00";
+        allTime[6]?.style.backgroundColor = "#94ff00";
     }
 }
 parseTime();
 
 const parseLesson = () => {
     if (currentDay.getDay() === 1)
-        allMonday[currentLesson - 1].style.backgroundColor = "#94ff00"
+        allMonday[currentLesson - 1]?.style.backgroundColor = "#94ff00"
     if (currentDay.getDay() === 2)
-        allTuesday[currentLesson - 1].style.backgroundColor = "#94ff00"
+        allTuesday[currentLesson - 1]?.style.backgroundColor = "#94ff00"
     if (currentDay.getDay() === 3)
-        allWednesday[currentLesson - 1].style.backgroundColor = "#94ff00"
+        allWednesday[currentLesson - 1]?.style.backgroundColor = "#94ff00"
     if (currentDay.getDay() === 4)
-        allThursday[currentLesson - 1].style.backgroundColor = "#94ff00"
+        allThursday[currentLesson - 1]?.style.backgroundColor = "#94ff00"
     if (currentDay.getDay() === 5)
-        allFriday[currentLesson - 1].style.backgroundColor = "#94ff00"
+        allFriday[currentLesson - 1]?.style.backgroundColor = "#94ff00"
 }
 
 parseLesson();
 
+
+
+const allTagA = document.querySelectorAll('a');
+const userIdInput = document.querySelector('#userId');
+
+const userId = localStorage.getItem("userId") || 0;
+userIdInput.value = userId
+
+userIdInput.addEventListener('change', (e) => {
+    localStorage.setItem("userId", e.target.value)
+});
+
+allTagA.forEach(item => {
+    item.setAttribute("href", `${item.getAttribute('href')}?authuser=${userId}`);
+});
 
 
 
