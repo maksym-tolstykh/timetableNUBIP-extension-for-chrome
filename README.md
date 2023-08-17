a4ab7afa0d09406e8d72d72eb04ad0f6f5792766
Tolstykh Maksym <mrymnik3d@gmail.com>
2023-08-17 12:10:24 +0300

    v1.1

---
+++ README.md
@@ -0,0 +1,31 @@
+![t](https://img.shields.io/github/languages/code-size/maxbutcher1/timetable-extension-for-chrome)
+# IFK timetable `v1.0.1`
+## Нажаль політика Google не дозволяє в магазин публікувати розширення в яких є посилання на сторонні ресурси,тобто які не є частиною розширення.
+
+![alt text](https://raw.githubusercontent.com/maxbutcher1/maxbutcher1.github.io/main/photoForExtension/screen.png)
+![alt text](https://raw.githubusercontent.com/maxbutcher1/maxbutcher1.github.io/main/photoForExtension/s7.png)
+
+### `1.Скачати архів`
+
+![alt text](https://raw.githubusercontent.com/maxbutcher1/maxbutcher1.github.io/main/photoForExtension/s2.png)
+Щоб скачати архів вам потібно нажати на зелену кнопку(Code => Download ZIP). Після цього розпакувати архів в папку яку ви точно не будете видаляти.
+
+### `2.Браузер Chrome`
+
+Перейти за посиланям яке приведе список де показуються всі ваші розширення для Chrome: chrome://extensions/
+Або прейти так як показано на малюнку:
+
+![s3](https://raw.githubusercontent.com/maxbutcher1/maxbutcher1.github.io/main/photoForExtension/s3.png)
+
+### `3.Додати розширення до свого браузеру`
+
+Далі потрібно перейти в режим розробника
+![s4](https://raw.githubusercontent.com/maxbutcher1/maxbutcher1.github.io/main/photoForExtension/s4.png)
+
+Потім нажати завантажити розпаковане розширення і в меню яке з'явиться вибрати папку з розширенням.
+![s5](https://raw.githubusercontent.com/maxbutcher1/maxbutcher1.github.io/main/photoForExtension/s5.png)
+
+
+Тепер все готов для користування розширенням яке сподіваюся допоможе швидше отримувати доступ до потрібних ресурсів коледжу.
+
+![s6](https://raw.githubusercontent.com/maxbutcher1/maxbutcher1.github.io/main/photoForExtension/s6.png)
---
+++ main.js
@@ -0,0 +1,139 @@
+//Контейнер з "Чисельник" або "Знаменник"
+const weekTypeContainer = document.querySelector('#weekType');
+//Змінна для типу "Чисельник" або "Знаменник"
+const startWeekType = localStorage.getItem("weekType") ? localStorage.getItem("weekType") : localStorage.setItem("weekType", "Чисельник");
+const canUpdate = localStorage.getItem("canUpdate") ? localStorage.getItem("canUpdate") : localStorage.setItem("canUpdate", false);
+//Список днів
+const days = ["Неділя", "Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота"];
+const currentDay = new Date();
+
+
+if (currentDay.getDay() === 0)
+    localStorage.setItem("canUpdate", true);
+
+if (currentDay.getDay() === 1 && canUpdate === "true") {
+
+    if (startWeekType === "Чисельник") {
+        localStorage.setItem("weekType", "Знаменик");
+        localStorage.setItem("canUpdate", false);
+    }
+    else if (startWeekType === "Знаменик") {
+        localStorage.setItem("weekType", "Чисельник");
+        localStorage.setItem("canUpdate", false);
+    }
+}
+
+
+weekTypeContainer.innerText = startWeekType;
+
+
+// Підсвічування поточного дня
+const daysInTable = document.querySelectorAll("th");
+daysInTable.forEach(item => {
+    if (days[currentDay.getDay()] === item.innerText) {
+        item.style.backgroundColor = "#94ff00"
+        console.log(item.innerText);
+    }
+
+
+})
+
+
+//Підсвічування часу
+const currentTime = new Date().getHours().toString();
+let currentLesson = 0;
+
+let allTime = [];
+let allMonday = [];
+let allTuesday = [];
+let allWednesday = [];
+let allThursday = [];
+let allFriday = [];
+
+const allTD = document.querySelectorAll("td").forEach(item => {
+    if (item.getAttribute("id")) {
+        if (item.getAttribute("id").includes("mo")) {
+            allMonday.push(item);
+        }
+        if (item.getAttribute("id").includes("tu")) {
+            allTuesday.push(item);
+        }
+        if (item.getAttribute("id").includes("we")) {
+            allWednesday.push(item);
+        }
+        if (item.getAttribute("id").includes("th")) {
+            allThursday.push(item);
+        }
+        if (item.getAttribute("id").includes("fr")) {
+            allFriday.push(item);
+        }
+    }
+    else {
+        allTime.push(item);
+    }
+
+});
+
+// parse time
+const parseTime = () => {
+    if (currentDay >= new Date(currentDay).setHours(8, 30, 0, 0) &&
+        currentDay <= new Date(currentDay).setHours(9, 50, 0, 0)) {
+        currentLesson = 1;
+        allTime[0].style.backgroundColor = "#94ff00";
+    }
+    else if (currentDay >= new Date(currentDay).setHours(10, 10, 0, 0) &&
+        currentDay <= new Date(currentDay).setHours(11, 30, 0, 0)) {
+        currentLesson = 2;
+        allTime[1].style.backgroundColor = "#94ff00";
+    }
+    else if (currentDay >= new Date(currentDay).setHours(11, 50, 0, 0) &&
+        currentDay <= new Date(currentDay).setHours(13, 10, 0, 0)) {
+        currentLesson = 3;
+        allTime[2].style.backgroundColor = "#94ff00";
+    }
+    else if (currentDay >= new Date(currentDay).setHours(13, 30, 0, 0) &&
+        currentDay <= new Date(currentDay).setHours(14, 50, 0, 0)) {
+        currentLesson = 4;
+        allTime[3].style.backgroundColor = "#94ff00";
+    }
+    else if (currentDay >= new Date(currentDay).setHours(15, 10, 0, 0) &&
+        currentDay <= new Date(currentDay).setHours(16, 30, 0, 0)) {
+        currentLesson = 5;
+        allTime[4].style.backgroundColor = "#94ff00";
+    }
+    else if (currentDay >= new Date(currentDay).setHours(16, 50, 0, 0) &&
+        currentDay <= new Date(currentDay).setHours(18, 10, 0, 0)) {
+        currentLesson = 6;
+        allTime[5].style.backgroundColor = "#94ff00";
+    }
+    else if (currentDay >= new Date(currentDay).setHours(18, 30, 0, 0) &&
+        currentDay <= new Date(currentDay).setHours(19, 50, 0, 0)) {
+        currentLesson = 7;
+        allTime[6].style.backgroundColor = "#94ff00";
+    }
+}
+parseTime();
+
+const parseLesson = () => {
+    if (currentDay.getDay() === 1)
+        allMonday[currentLesson - 1].style.backgroundColor = "#94ff00"
+    if (currentDay.getDay() === 2)
+        allTuesday[currentLesson - 1].style.backgroundColor = "#94ff00"
+    if (currentDay.getDay() === 3)
+        allWednesday[currentLesson - 1].style.backgroundColor = "#94ff00"
+    if (currentDay.getDay() === 4)
+        allThursday[currentLesson - 1].style.backgroundColor = "#94ff00"
+    if (currentDay.getDay() === 5)
+        allFriday[currentLesson - 1].style.backgroundColor = "#94ff00"
+}
+
+parseLesson();
+
+
+
+
+
+
+
+
+
